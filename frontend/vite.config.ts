cdfd97577c9898dce624f84e0e60628a0609c02d
arpusauri <aryapurnama877@gmail.com>
2026-06-19 05:34:30 +0700

    feat: integrasi halaman pricing frontend dan backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api/pricing': {
+        target: 'http://localhost:8080', 
+        changeOrigin: true,
+        // 👇 BARIS INI YANG PALING PENTING:
+        rewrite: (path) => path.replace(/^\/api\/pricing/, '/pricing')
+      },
+    }
+  }
 })
